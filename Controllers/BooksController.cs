66924788d5c0854840edc97f9bff9d44f5436401
Vasily Pascal <vasilypascal@gmail.com>
2021-07-04 20:20:30 +0300

    Added CRUD endpoints

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -31,5 +31,38 @@ namespace BookStoreAPI.Controllers
         {
             return await _bookRepository.Get(id);
         }
+
+        [HttpPost]
+        public async Task<ActionResult<Book>> GetBook([FromBody]Book book)
+        {
+            var newBook = await _bookRepository.Create(book);
+            return CreatedAtAction(nameof(GetBooks), new { id = newBook.Id });
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> PutBooks(int id, [FromBody] Book book)
+        {
+            if (id != book.Id)
+            {
+                return BadRequest();
+            }
+
+            await _bookRepository.Update(book);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var bookToDelete = await _bookRepository.Get(id);
+            if (bookToDelete == null)
+            {
+                return NotFound();
+            }
+
+            await _bookRepository.Delete(bookToDelete.Id);
+            return NoContent();
+        }
     }
 }
--- Models/Book.cs
+++ Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,19 @@ namespace BookStoreAPI.Models
 {
     public class Book
     {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        public string Description { get; set; }
+
+        public int ReleaseYear { get; set; }
+
+        public short Rating { get; set; } 
     }
 }
